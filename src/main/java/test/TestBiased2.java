0c398ff6b4ad29c1719412c1e5a729540dca8ce8
soul9779 <17822006638@163.com>
2020-02-22 00:01:23 +0800

    aaa

--- src/main/java/test/TestBiased2.java
+++ src/main/java/test/TestBiased2.java
@@ -1,6 +1,10 @@
 package test;
 
 
+import org.openjdk.jol.info.ClassLayout;
+
+import java.util.Vector;
+
 /**
  *批量重偏向
  * 如果对象虽然被多个线程访问,但没有竞争,这时偏向了线程 T1 的对象仍有机会重新偏向 T2,重偏向会重置对象
@@ -11,4 +15,54 @@ package test;
  * */
 
 public class TestBiased2 {
+    public static void main(String[] args) {
+        Vector<Cat> cats = new Vector<>();
+        Thread t1 = new Thread(()->{
+            for (int i = 0;i<30;i++){
+                Cat cat = new Cat();
+                cats.add(cat);
+                synchronized (cat){
+                    System.out.println(ClassLayout.parseInstance(cat).toPrintable());
+                }
+            }
+            synchronized (cats){
+                cats.notify();
+            }
+        });
+        t1.start();
+
+        Thread t2 = new Thread(()->{
+            synchronized (cats){
+                try {
+                    cats.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("=================================================================================================================================================>");
+            System.out.println("=================================================================================================================================================>");
+            System.out.println("=================================================================================================================================================>");
+            for (int i = 0;i<30;i++){
+                Cat cat = cats.get(i);
+                if (i==19){
+                    System.out.println("=====================================开始重偏向==========================================");
+                }
+                System.out.println(i+"\t"+ClassLayout.parseInstance(cat).toPrintable());
+
+                synchronized (cat){
+                    System.out.println(i+"\t"+"synchronized"+"\t"+ClassLayout.parseInstance(cat).toPrintable());
+                }
+                System.out.println(i+"\t"+"finished"+"\t"+ClassLayout.parseInstance(cat).toPrintable());
+
+            }
+            synchronized (cats){
+                cats.notify();
+            }
+        });
+        t2.start();
+
+    }
+}
+class Cat{
+
 }
